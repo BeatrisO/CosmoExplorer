0c72b085bacd13456a1f7ae2f73dedff70bc66f7
BeatrisO <beatris_oliveira97@hotmail.com>
2026-01-15 14:41:39 -0300

    Cria data classes Rocket e RocketsResponse, e método GET para rockets

--- app/src/main/java/com/example/cosmoexplorer/data/model/SpaceXResponse.kt
+++ app/src/main/java/com/example/cosmoexplorer/data/model/SpaceXResponse.kt
@@ -1,2 +1,9 @@
 package com.example.cosmoexplorer.data.model
 
+data class Rocket (
+    val name: String
+)
+
+data class RocketsResponse(
+    val rockets: List<Rocket>
+)
--- app/src/main/java/com/example/cosmoexplorer/data/remote/ApiService.kt
+++ app/src/main/java/com/example/cosmoexplorer/data/remote/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.space.data.remote
 
+import com.example.cosmoexplorer.data.model.RocketsResponse
 import com.example.space.data.model.ApodResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,4 +10,7 @@ interface ApiService {
     @GET("planetary/apod")
     suspend fun getApod(
         @Query("api_key") apiKey: String): ApodResponse
+
+    @GET("rockets")
+    suspend fun get(): RocketsResponse
 }
