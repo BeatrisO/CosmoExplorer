104ae1a9923c5ff2eb7ba28001ff804c7a554a66
BeatrisO <beatris_oliveira97@hotmail.com>
2026-01-29 14:31:39 -0300

    Ajusta RocketDetailViewModel.kt

--- app/src/main/java/com/example/cosmoexplorer/presentation/viewmodel/RocketDetailViewModel.kt
+++ app/src/main/java/com/example/cosmoexplorer/presentation/viewmodel/RocketDetailViewModel.kt
@@ -1,17 +1,49 @@
 package com.example.cosmoexplorer.presentation.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.example.cosmoexplorer.data.model.Rocket
+import androidx.lifecycle.viewModelScope
+import com.example.cosmoexplorer.data.repository.SpaceXRepository
 import com.example.cosmoexplorer.presentation.screens.spacex.detailscreen.RocketDetailUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class RocketDetailViewModel : ViewModel() {
+class RocketDetailViewModel(
+    private val repository: SpaceXRepository = SpaceXRepository(),
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
-    private val state = MutableStateFlow(RocketDetailUiState())
+    private val state = MutableStateFlow(RocketDetailUiState(isLoading = true))
     val uiState: StateFlow<RocketDetailUiState> = state
 
-    fun setRocket(rocket: Rocket) {
-        state.value = RocketDetailUiState(rocket = rocket)
+    private val rocketName: String =
+        savedStateHandle["rocketName"] ?: ""
+
+    init {
+        loadRocket()
+    }
+
+    private fun loadRocket() {
+        viewModelScope.launch {
+            state.value = RocketDetailUiState(isLoading = true)
+
+            try {
+                val rocket = repository
+                    .getRockets()
+                    .first { it.name == rocketName }
+
+                state.value = RocketDetailUiState(
+                    isLoading = false,
+                    rocket = rocket
+                )
+
+            } catch (e: Exception) {
+                state.value = RocketDetailUiState(
+                    isLoading = false,
+                    errorMessage = "Error loading rocket"
+                )
+            }
+        }
     }
 }
