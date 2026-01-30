57b060f9669894056221264a7564ccc58ab9ca95
BeatrisO <beatris_oliveira97@hotmail.com>
2026-01-30 15:28:45 -0300

    Melhora a MainActivity.kt

--- app/src/main/java/com/example/cosmoexplorer/MainActivity.kt
+++ app/src/main/java/com/example/cosmoexplorer/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.cosmoexplorer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.cosmoexplorer.presentation.components.BottomItem
 import com.example.cosmoexplorer.presentation.components.BottomNavigationBar
 import com.example.cosmoexplorer.presentation.screens.apod.ApodScreen
@@ -18,47 +21,63 @@ import com.example.cosmoexplorer.presentation.screens.spacex.detailscreen.Rocket
 import com.example.cosmoexplorer.presentation.screens.spacex.listscreen.SpacexScreen
 import com.example.cosmoexplorer.presentation.theme.CosmoExplorerTheme
 
-
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
-            CosmoExplorerTheme {
-                val navController = rememberNavController()
-                Scaffold(
-                    bottomBar = {
-                        BottomNavigationBar(navController)
-                    }
-                ) { innerPadding ->
-                    NavHost(
-                        navController = navController,
-                        startDestination = BottomItem.Apod.route,
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        composable(BottomItem.Apod.route) {
-                            ApodScreen()
-                        }
+            CosmoExplorerApp()
+        }
+    }
+}
+@Composable
+fun CosmoExplorerApp() {
+    CosmoExplorerTheme {
 
-                        composable(BottomItem.Spacex.route) {
-                            SpacexScreen(
-                                onRocketClick = { rocketName ->
-                                    navController.navigate("rocket_detail/$rocketName")
-                                }
-                            )
-                        }
+        val navController = rememberNavController()
 
-                        composable(
-                            route = "rocket_detail/{rocketName}"
-                        ) {
-                            RocketDetail()
-                        }
+        Scaffold(
+            bottomBar = {
+                BottomNavigationBar(navController)
+            }
+        ) { innerPadding ->
 
-                        composable(BottomItem.Settings.route) {
-                            SettingsScreen()
+            NavHost(
+                navController = navController,
+                startDestination = BottomItem.Apod.route,
+                modifier = Modifier.padding(innerPadding)
+            ) {
+
+                composable(BottomItem.Apod.route) {
+                    ApodScreen()
+                }
+
+                composable(BottomItem.Spacex.route) {
+                    SpacexScreen(
+                        onRocketClick = { rocketId ->
+                            navController.navigate("rocketDetail/$rocketId")
                         }
-                    }
+                    )
+                }
+
+                composable(
+                    route = "rocketDetail/{rocketId}",
+                    arguments = listOf(
+                        navArgument("rocketId") { type = NavType.StringType }
+                    )
+                ) { backStackEntry ->
+
+                    val rocketId =
+                        backStackEntry.arguments?.getString("rocketId") ?: return@composable
+
+                    RocketDetail(rocketId = rocketId)
+                }
+
+                composable(BottomItem.Settings.route) {
+                    SettingsScreen()
                 }
             }
         }
