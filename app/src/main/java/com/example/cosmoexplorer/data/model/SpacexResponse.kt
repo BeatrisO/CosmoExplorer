a977bef2b26e219f495f1897e19ab3d4abb0066b
BeatrisO <beatris_oliveira97@hotmail.com>
2026-02-05 14:16:51 -0300

    Inclui status de atividade, primeiro voo e taxa de sucesso Adiciona custo por lançamento e link da Wikipedia

--- app/src/main/java/com/example/cosmoexplorer/data/model/SpacexResponse.kt
+++ app/src/main/java/com/example/cosmoexplorer/data/model/SpacexResponse.kt
@@ -4,5 +4,12 @@ data class Rocket(
     val id: String,
     val name: String,
     val description: String,
+
+    val active: Boolean,
+    val first_flight: String,
+    val success_rate_pct: Int,
+    val cost_per_launch: Long,
+    val wikipedia: String,
+
     val flickr_images: List<String>
 )
