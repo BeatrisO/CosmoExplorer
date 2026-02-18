4bd24a816aaa85daba87d39b1a4dfdcaf6062913
BeatrisO <beatris_oliveira97@hotmail.com>
2026-02-18 15:36:55 -0300

    Cria RocketListUiModel.kt

---
+++ app/src/main/java/com/example/cosmoexplorer/presentation/screens/spacex/listscreen/RocketListUiModel.kt
@@ -0,0 +1,8 @@
+package com.example.cosmoexplorer.presentation.screens.spacex.listscreen
+
+data class RocketListUiModel(
+    val id: String,
+    val name: String,
+    val statusText: String,
+    val imageUrl: String?
+)
