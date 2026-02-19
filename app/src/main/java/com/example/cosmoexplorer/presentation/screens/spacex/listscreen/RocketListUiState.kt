0a6eec262cd83c8179fa7e639c5916f609560950
BeatrisO <beatris_oliveira97@hotmail.com>
2026-02-19 15:55:37 -0300

    Corrige RocketListUiState.kt

--- app/src/main/java/com/example/cosmoexplorer/presentation/screens/spacex/listscreen/RocketListUiState.kt
+++ app/src/main/java/com/example/cosmoexplorer/presentation/screens/spacex/listscreen/RocketListUiState.kt
@@ -1,9 +1,8 @@
 package com.example.cosmoexplorer.presentation.screens.spacex.listscreen
 
-import com.example.cosmoexplorer.data.model.Rocket
 
 data class RocketListUiState(
     val isLoading: Boolean = false,
     val errorMessage: String? = null,
-    val rockets: List<Rocket> = emptyList()
+    val rockets: List<RocketListUiModel> = emptyList()
 )
